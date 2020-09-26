40e31394cf3123b4f43e185ddde49fd6f3114554
saqfish <saqfish@gmail.com>
2020-09-26 02:40:55 -0500

    border patch applied

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -22,3 +22,6 @@ static unsigned int columns    = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
