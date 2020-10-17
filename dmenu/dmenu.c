4f99768b31dd4b27a69b00b942260ee987284151
saqfish <saqfish@gmail.com>
2020-10-17 07:36:30 -0500

    spacing

--- dmenu/dmenu.c
+++ dmenu/dmenu.c
@@ -623,7 +623,7 @@ setup(void)
 	utf8 = XInternAtom(dpy, "UTF8_STRING", False);
 
 	/* calculate menu geometry */
-	bh = drw->fonts->h + 2;
+	bh = drw->fonts->h + 5;
 	lines = MAX(lines, 0);
 	mh = (lines + 1) * bh;
 	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
