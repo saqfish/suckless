7afd9c0fce51dd64bc5d8fe7f02fddd4348513e2
saqfish <saqfish@gmail.com>
2020-11-12 04:16:17 -0600

    bg

--- st/config.h
+++ st/config.h
@@ -120,6 +120,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+	"#0a1016",
 };
 
 
@@ -128,7 +129,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
