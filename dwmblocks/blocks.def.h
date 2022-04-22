59c7240f5694e8e7b7e92f3d078352c62d61eec3
saqfish <saqfish@gmail.com>
2022-04-22 15:56:43 -0500

    readd dwmblocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,8 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"Bat0:", "cat /sys/class/power_supply/BAT0/capacity", 30,		0},
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
