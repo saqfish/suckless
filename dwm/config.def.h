27405d99e23d4fc61de7fbe5bce90a776c963537
saqfish <saqfish@gmail.com>
2022-04-22 15:47:13 -0500

    add autostart programs

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -23,6 +23,14 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const char *const autostart[] = {
+	"chromium", NULL,
+	"st", NULL,
+	"dwmblocks", NULL,
+	"volumeicon", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
@@ -32,8 +40,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",      NULL,       NULL,      1 << 8,       0,           -1 },
+	{ "Chromium",  NULL,       NULL,      0,       	    0,           -1 },
 };
 
 /* layout(s) */
