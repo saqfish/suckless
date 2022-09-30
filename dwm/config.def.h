623c3550e9609cf12d08013de675a322ced6b761
saqfish <saqfish@gmail.com>
2022-09-30 09:28:18 -0500

    switch to firefox

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 
 /* autostart programs */
 static const char *const autostart[] = {
-	"chromium", NULL,
+	"firefox", NULL,
 	"st", "-f", termfont, "-t", "Session", NULL,
 	"sbar", NULL,
 	"volumeicon", NULL,
@@ -61,7 +61,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",      NULL,       NULL,      1 << 8,       0,           -1 },
-	{ "Chromium",  NULL,       NULL,      0,       	    0,           -1 },
+	{ "Firefox",   NULL,       NULL,      0,       	    0,           -1 },
 	{ "st",        NULL,       "Session", 2,       	    0,           -1 },
 	{ "st",        NULL,       "Terminal",0,            1,           -1 },
 };
